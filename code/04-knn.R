2dd99feb18a85cb9766ffb0fe5e72d8a1f70a8a7
Achilleas Kouventaras <achilleas.kouventaras@gmail.com>
2024-05-06 11:50:36 +0100

    add knn script

---
+++ code/04-knn.R
@@ -0,0 +1,10 @@
+
+#Unlike regular plots with one axis pair, biplots have two sets. 
+
+#The first set (usually the left-most and bottom vertical and horizontal axes) 
+#shows principal component (PC) scores for subjects.
+#The second set (usually the right most and top vertical and horizontal axes) 
+#displays variable loadings on PCs. 
+#These are visualized by arrows originating from the plot center, 
+#with lengths proportional to the variable's weight on each PC.
+
