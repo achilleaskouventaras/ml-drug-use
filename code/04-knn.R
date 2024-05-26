877f77c839858aa525501cc7cfadc492c36a621b
Achilleas Kouventaras <achilleas.kouventaras@gmail.com>
2024-05-26 15:19:52 +0100

    minor changes to kkn base code - needs commentary

--- code/04-knn.R
+++ code/04-knn.R
@@ -6,6 +6,7 @@ pacman::p_load(class)
 
 #read the data
 train <- read.csv("data/patient-data-train.csv") #read train data
+head(train, 5) #variable X can be dropped
 train <- train[,c(2:13)] #create a new data frame to work with (X is dropped)
 
 valid <- read.csv("data/patient-data-valid.csv") #read train data
@@ -15,15 +16,9 @@ test <- read.csv("data/patient-data-test.csv") #read train data
 test <- test[,c(2:13)] #create a new data frame to work with (X is dropped)
 
 
-
-
-head(train, 5) #variable X can be dropped
-df <- train[,c(2:13)] #create a new data frame to work with (X is dropped)
-summary(df) #summary statistics
-
-
-
+# fit k-nearest neighbours models for a variety of k
 corr.class.rate <- numeric(200) #values of k
+
 for(k in 1:200)
 {
   pred.class <- knn(train[, -12], valid[, -12], train[, 12], k = k)
@@ -34,6 +29,7 @@ plot(c(1:200), corr.class.rate, type = "l",
      main = "Correct Classification Rates for the validation data for a range of k",
      xlab = "k", ylab = "Correct Classification Rate", cex.main = 0.7)
 
+#evaluate  performance on the validation data to select the optimal value of k
 corr.class.rate #correct classification rate
 which.max(corr.class.rate) #identify max correct classification rate 
 #107
@@ -42,6 +38,5 @@ which.max(corr.class.rate) #identify max correct classification rate
 #### Prediction ####
 
 pred <- knn(train[,-12], test[,-12], train[,12], k = 107)
+#estimate of the future performance for a model with the optimal k using the test data
 sum((pred == test$Class))/length(pred) #0.788 correct classification rate
-
-
